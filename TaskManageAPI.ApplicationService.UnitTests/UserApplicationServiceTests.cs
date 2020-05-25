693c774cc6eef3f094c6d9ece0d40ecac4cbc3ec
tadashi.s <tadsuzuki@tech.softbank.co.jp>
2020-05-25 21:26:02 +0900

    テストを追加

---
+++ TaskManageAPI.ApplicationService.UnitTests/UserApplicationServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManageAPI.ApplicationService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManageAPI.ApplicationService.Tests
+{
+    [TestClass()]
+    public class UserApplicationServiceTests
+    {
+        [TestMethod()]
+        public void CreateTest_case_false()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TaskManageAPI.DomainService.UnitTests/UserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManageAPI.DomainService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using TaskManageAPI.DomainService.Interface;
+using TaskManageAPI.DomainModel;
+
+namespace TaskManageAPI.DomainService.Tests
+{
+    [TestClass()]
+    public class UserServiceTests
+    {
+        [TestMethod()]
+        public void IsDuplicatedTest_case_false()
+        {
+            // Arrange
+            var moc = new Mock<IUserRepository>();
+            moc.Setup(repo => repo.Get())
+                .Returns(new List<User>() { 
+                    new User() { 
+                        email = "ex@example.com" 
+                    }
+                });
+
+            // Act
+            var result = new UserService(moc.Object).IsDuplicated("false@example.com");
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
